593c3f25f0a9659d241f88c3844dc8d8ce9fdc8a
Muhammad Faizan <fzanrmzan@gmail.com>
2025-02-09 01:43:14 -0800

    Fixed homepage in package.json

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,4 +1,6 @@
 import CustomCursor from '../components/shared/AnimatedCursor'
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
+
 
 function MyApp({ Component, pageProps }) {
   return (
